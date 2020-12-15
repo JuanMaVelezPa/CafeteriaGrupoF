535d64288bf8c3093a63d329efc37e6620d73417
JuanMaVelezPa <velezmj@uninorte.edu.co>
2020-12-15 18:24:19 -0500

    errores

--- app.py
+++ app.py
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-from flask import Flask, render_template, request, flash,  redirect, session, g, url_for, send_file
-from db import get_db
-
-=======
 from flask import Flask, render_template, request, flash
 from db import get_db, close_db
 from werkzeug.security import generate_password_hash,check_password_hash
->>>>>>> master
+
 import utils
 import os 
 import yagmail as yagmail
@@ -23,48 +18,11 @@ def myHome():
 
 @app.route('/login/', methods=('GET','POST'))
 def login():
-<<<<<<< HEAD
-    try:
-=======
     try: 
->>>>>>> master
         if request.method == 'POST':
             db = get_db()
             username = request.form['username']
             password = request.form['password']
-<<<<<<< HEAD
-            error = None
-
-            if not utils.isUsernameValid(username):
-                error = "Usuario - Datos no validos"
-                flash(error)
-                return render_template('login.html')
-
-            if not utils.isPasswordValid(password):
-                error = 'Contraseña - Datos no validos'
-                flash(error)
-                return render_template('login.html')
-
-            print("Usuario: " + username + " clave: " + password)
-
-            user = db.execute('SELECT id_rol FROM usuarios WHERE usuario=? AND contraseña=?', (username, password)).fetchone()
-            
-            
-            if user is None:
-                error = "usuario y/o contraseña invalidos"
-                flash(error)
-            if user[3]=="A":
-                return render_template("admin.html")
-            if user[3]=="U":
-                return render_template("store.html")
-            else:
-                return render_template("login.html")
-
-        return render_template("login.html")
-
-    except TypeError as e:
-        print("ocurrio un error"+e)
-=======
 
             if not username:
                 error= "Debes ingresar el usuario"
@@ -96,7 +54,6 @@ def login():
         return render_template('login.html')
     except TypeError as e:
         print("Ocurrio un error ",e)
->>>>>>> master
         return render_template('login.html')
 
 @app.route('/store/')
@@ -141,16 +98,12 @@ def passwordLost():
 
 @app.route('/register/', methods=('GET','POST'))
 def register():
-    # try:
+    try:
         if request.method == 'POST':
             username = request.form['username']
             password = request.form['password']
             email = request.form['email']
             rol = request.form['rol']
-<<<<<<< HEAD
-
-=======
->>>>>>> master
             error = None
             db= get_db()
 
@@ -173,12 +126,6 @@ def register():
                 flash(error)
                 return render_template('register.html')
 
-<<<<<<< HEAD
-            if db.execute('SELECT usuario FROM usuarios WHERE correo=?', (email,)).fetchone() is not None:
-                error = "El correo ya existe"+format(email)
-                flash(error)
-                return render_template("login.html")
-=======
             if db.execute('SELECT * FROM usuarios WHERE usuario=? OR correo=?',(username,email)).fetchone() is not None:
                 error= "El usuario o correo electronico ya estan registrados"
                 flash(error)
@@ -187,35 +134,21 @@ def register():
             db.execute('INSERT INTO usuarios (usuario,contraseña,correo,rol,activo) VALUES (?,?,?,?,1)',(username,hash_password,email,rol))
             db.commit()
 
->>>>>>> master
 
             serverEmail = yagmail.SMTP('CafeteriaAromaMisionTic@gmail.com', 'Maracuya123')
 
             serverEmail.send(to=email, subject='Tu cuenta '+username+" en Cafeteria Aroma ha sido creada",
                              contents='Bienvenido')
 
-<<<<<<< HEAD
-            db.execute('INSERT INTO usuarios (usuario, contraseña, correo, id_rol) VALUES(?,?,?,?)', (username, password, email, rol)).fetchone()
-            
-            db.commit()
-            
-            flash('Revisa tu correo para activar tu cuenta')
-=======
             flash('Revisa tu correo ')
->>>>>>> master
 
             return render_template('login.html')
 
         return render_template('register.html')
-<<<<<<< HEAD
-    # except Exception as e:
-    #     #print("Ocurrio un eror:", e)
-    #     return render_template('register.html')
-=======
+
     except Exception as e:
-        print("Ocurrio un eror:", e)
+        #print("Ocurrio un eror:", e)
         return render_template('register.html')
->>>>>>> master
 
 @app.route('/passwordLost/', methods=('GET','POST'))
 def revision():
--- db.py
+++ db.py
@@ -2,29 +2,15 @@ import sqlite3
 from sqlite3 import Error
 from flask import g
 
-<<<<<<< HEAD
-def get_db():
-    try:
-        if "db" not in g:
-            g.db = sqlite3.connect("cafeteria.db")
-=======
 def get_db():#Conecta base de datos
     try:
         if 'db' not in g:
             g.db = sqlite3.connect('cafeteria.db')
->>>>>>> master
             return g.db
     except Error:
         print(Error)
 
-<<<<<<< HEAD
-def close_db():
-    db = db.pop("cafeteria.db")
-    if db is not None:
-        db.close()
-=======
 def close_db():#Desconecta base de datos
     db = g.pop('db',None)
     if db is not None:
         db.close()
->>>>>>> master
